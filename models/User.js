7e4e6b3928e80312643904c747c438e174c5d8be
Jacob H <jacob.hannah164@gmail.com>
2022-07-28 10:22:53 -0400

    created a single post template for posting and upvoting posts/ fixed logout functionality

--- models/User.js
+++ models/User.js
@@ -1,87 +1,63 @@
-const {Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-//Create the User model
-class User extends Model{
-    checkPassword(loginPw){
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+// create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
-//define table columns and configuration
+// create fields/columns for User model
 User.init(
-    {
-        id:{
-            //use the sequelize data type object provide what type of data it is
-            type: DataTypes.INTEGER,
-
-            //do not allow empty or null ids
-            allowNull: false,
-            
-            //instruct this as the Primary Key
-            primaryKey: true,
-
-            //turn on auto increment
-            autoIncrement: true
-        },
-        username:{
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email:{
-            type: DataTypes.STRING,
-            allowNull: false,
-
-            //no duplicate values can be input into the table
-            unique:true,
-
-            //if allNull is false then we can run through validators before creating the table data
-            validate:{
-                isEmail: true
-            }
-        },
-        password:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate:{
-                //set a minimum length of 4 characters
-                len: [4]
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    {
-        hooks: {
-            //setup beforeCreate lifecycle hook functionality
-            async beforeCreate(newUserData){
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-
-            //setup beforeUpdate lifecycle hook functionality
-            async beforeUpdate(updatedUserData){
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10)
-                return updatedUserData;
-            }
-
-        },
-        //Table configuration options go here
-        
-        //pass in the connection
-        sequelize,
-
-        //dont auto create timestamp fields
-        timestamps: false,
-
-        //dont pluralize name of database table
-        freezeTableName:true,
-
-        //use underscores instead of camel-casing
-        underscored:true,
-
-        //make the model name stay lowercased in the database
-        modelName: 'user'
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
-)
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
 
 module.exports = User;
